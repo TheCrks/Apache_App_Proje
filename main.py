90144a9ca5dce84dc4ae83dd6817736d26a59c12
UmutEnesUzun <uuzun@marun.edu.tr>
2023-06-06 16:17:25 +0300

    feat: add control mechanism to chechk DB changes

--- main.py
+++ main.py
@@ -1,45 +1,87 @@
+from json import JSONEncoder
+
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-from urllib.parse import quote_plus
-#def get_database():
-    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-#    CONNECTION_STRING = "mongodb+srv://uuzun:<password>@cluster0.khd9rlb.mongodb.net/?retryWrites=true&w=majority"
+import time
+import json
 
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-#    client = MongoClient(CONNECTION_STRING)
-
-    # Create the database for our example (we will use the same database throughout the tutorial
-#    return client['user_shopping_list']
+global db, previous_db, client
 
 
-# This is added so that many files can reuse the function get_database()
-#if __name__ == "__main__":
-    # Get the database
- #   dbname = get_database()
-username = quote_plus('uuzun')
-password = quote_plus('162Crks%2F*-123')
-#cluster = '<clusterName>'
-#authSource = '<authSource>'
-#authMechanism = '<authMechanism>'
-#uri = 'mongodb+srv://' + username + ':' + password + '@' + cluster + '/?authSource=' + authSource + '&authMechanism=' + authMechanism
+def getDb():
+    global db, previous_db, client
+    uri = "mongodb+srv://uuzun:EwuvFhEZNKySGgBE@cluster0.khd9rlb.mongodb.net/?retryWrites=true&w=majority"
+
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    # Send a ping to confirm a successful connection
+    try:
+        db = client["sample_db_project"]
+    except Exception as e:
+        print(e)
 
 
-
-uri = "mongodb+srv://uuzun:EwuvFhEZNKySGgBE@cluster0.khd9rlb.mongodb.net/?retryWrites=true&w=majority"
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+getDb()
+previous_db =[]
 
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+def previous(filename):
+    global db, previous_db
+    export_db_to_json(db, "db.json")
+    with open(filename, "r") as json_file:
+        previous_db = json_file.readlines()
 
-db = client["sample_analytics"]
-col = db["customers"]
-doc = col.find()
-for x in doc:
-    print(x)
+    previous_db = [line.strip() for line in previous_db]
+
+
+def export_db_to_json(db, filename):
+    open(filename, "w")
+    # Fetch all documents from the specified collection
+    collections = db.list_collection_names()
+    document = []
+    for x in collections:
+        col = db[x]
+        doc = col.find()
+        document.append(doc)
+    # Write the documents to a JSON file
+
+    with open(filename, "a") as json_file:
+        for doc in document:
+            for y in doc:
+                serialized_document = str(y)
+                json_file.write(serialized_document + "\n")
+
+export_db_to_json(db, "db.json")
+previous("db.json")
+
+
+def compareDB():
+    global previous_db, db
+    string = []
+    result = 1
+    collections = db.list_collection_names()
+    for x in collections:
+        collection = db[x]
+        document = collection.find()
+        i = 0
+        for y in document:
+            string.append(str(y))
+
+    i = 0
+
+    for s in string:
+        if s != previous_db[i]:
+            print(s + " != " + previous_db[i])
+            result += 1
+        i+=1
+    return result
+
+
+while 1:
+    if compareDB() != 1:
+        print("Database Changed")
+    else:
+        print("No changes")
+    previous("db.json")
+    time.sleep(10)
