2d67bc6a164c44a3f540a4418e571a7369f53de5
UmutEnesUzun <uuzun@marun.edu.tr>
2023-06-09 10:31:40 +0300

    fix: now can detect new collection and documents

--- main.py
+++ main.py
@@ -24,14 +24,15 @@ def getDb():
 
 getDb()
 previous_db =[]
+previous_collections = ""
 
 
 def previous(filename):
-    global db, previous_db
+    global db, previous_db, previous_collections
     export_db_to_json(db, "db.json")
     with open(filename, "r") as json_file:
         previous_db = json_file.readlines()
-
+    previous_collections = db.list_collection_names()
     previous_db = [line.strip() for line in previous_db]
 
 
@@ -57,11 +58,20 @@ previous("db.json")
 
 
 def compareDB():
-    global previous_db, db
+    global previous_db, db, previous_collections
     string = []
     result = 1
     collections = db.list_collection_names()
+    j = 0
     for x in collections:
+        try:
+            if x != previous_collections[j]:
+                result += 1
+        except Exception as e:
+            print(e)
+            print("Error Possible new collection. Returning -1")
+            return -1
+        j += 1
         collection = db[x]
         document = collection.find()
         i = 0
@@ -71,10 +81,15 @@ def compareDB():
     i = 0
 
     for s in string:
-        if s != previous_db[i]:
-            print(s + " != " + previous_db[i])
-            result += 1
-        i+=1
+        try:
+            if s != previous_db[i]:
+                print(s + " != " + previous_db[i])
+                result += 1
+        except Exception as e:
+            print(e)
+            print("Error Possible new document. Returning -1")
+            return -1
+        i += 1
     return result
 
 
